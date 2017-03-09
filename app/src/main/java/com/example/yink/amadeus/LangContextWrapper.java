0a329db4a33af902b78554e0d9bc40586ade3036
Snek <rip95_95@mail.ru>
2017-03-09 04:26:12 +0300

    Implement voice recognition language switch

--- app/src/main/java/com/example/yink/amadeus/LangContextWrapper.java
+++ app/src/main/java/com/example/yink/amadeus/LangContextWrapper.java
@@ -7,10 +7,8 @@ import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
-import android.content.res.Resources;
 import android.os.Build;
 import android.preference.PreferenceManager;
-import android.util.DisplayMetrics;
 
 import java.util.Locale;
 
