b56ad484d0a88c86b08bef4780ef8bad0062843f
RIP95 <rip95_95@mail.ru>
2017-03-19 20:32:22 +0300

    WIP: Alarm feature improvements

--- app/src/main/java/com/example/yink/amadeus/AlarmReceiver.java
+++ app/src/main/java/com/example/yink/amadeus/AlarmReceiver.java
@@ -4,21 +4,35 @@ import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v4.content.WakefulBroadcastReceiver;
+import android.util.Log;
 
 public class AlarmReceiver extends WakefulBroadcastReceiver {
 
-    private static Ringtone ringtone = null;
+    static MediaPlayer m;
 
     @Override
-    public void onReceive(final Context context, Intent intent) {
-        // TODO: Should we use ringtone or switch to MediaPlayer with loop?
-        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
-        ringtone = RingtoneManager.getRingtone(context, alarmUri);
-        ringtone.play();
+    public void onReceive(Context context, Intent intent) {
+        int[] ringtones = {
+                R.raw.ringtone_gate_of_steiner, R.raw.ringtone_village,
+                R.raw.ringtone_beginning_of_fight, R.raw.ringtone_easygoingness,
+                R.raw.ringtone_reunion, R.raw.ringtone_precaution,
+                R.raw.ringtone_over_the_sky
+        };
+
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+        int index = Integer.parseInt(settings.getString("ringtone", "0"));
+
+        m = MediaPlayer.create(context, ringtones[index]);
+
+        m.setLooping(true);
+        m.start();
 
         //this will send a notification message
         ComponentName comp = new ComponentName(context.getPackageName(),
@@ -28,16 +42,16 @@ public class AlarmReceiver extends WakefulBroadcastReceiver {
     }
 
     public static void stopRingtone() {
-        if (ringtone != null) {
-            if (ringtone.isPlaying()) {
-                ringtone.stop();
+        if (m != null) {
+            if (m.isPlaying()) {
+                m.stop();
             }
         }
     }
 
     public static boolean isPlaying() {
-        if (ringtone != null) {
-            return ringtone.isPlaying();
+        if (m != null) {
+            return m.isPlaying();
         }
         return false;
     }
